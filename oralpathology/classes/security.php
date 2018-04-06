329c6b85184723dc3273400297db54e86d781611
AAmora1913 <aamora1913@yahoo.com>
2018-04-06 11:04:32 -0400

    updated comments

--- oralpathology/classes/security.php
+++ oralpathology/classes/security.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 // echo("CURRENT SESSION: " . $_SESSION["drupalUserID"]);
+
+/* checks to see if user ID is blank or 'invalid'. Redirects to authfailed.php if so*/
 if(($_SESSION['drupalUserID'] == "") || ($_SESSION['drupalUserID'] == "INVALID")){
 	// echo("Not logged in!");
 	header("Location: authfailed.php");
